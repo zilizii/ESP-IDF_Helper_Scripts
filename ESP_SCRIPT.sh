e4c41fc47452edbd29fef2fb8f7ad7cf50afa09f
Istvan Zilizi <istvan.zilizi@outlook.com>
2025-03-13 21:36:13 +0100

    feat: Select ttyUSB port feature added

--- ESP_SCRIPT.sh
+++ ESP_SCRIPT.sh
@@ -1,16 +1,45 @@
 #!/bin/bash
 
+function checkTTY(){
+choise=$(ls -1 /dev | grep ttyUSB*)
+line="${choise//$'\n'/ }"
+eval "choices=($line)"
+choices+=("re-check")
+choices+=("exit")
 
+select answer in "${choices[@]}"; do
+  for item in "${choices[@]}"; do
+    if [[ $item == $answer ]]; then
+      break 2
+    fi
+  done
+done
 
+if [[ $answer == "re-check" ]]; then
+	checkTTY
+elif [[ $answer == "exit" ]]; then
+	echo "Good Luck!"
+	exit 0
+else
+	echo "Port selected ..."
+fi
+}
+
+echo "Setting the Environment..."
 . esp/esp-idf/export.sh
+echo "Entering the folder."
 cd esp-workspace/RTC_MS/
 
 read -p "Would you like to start monitor a port? (Y/N): " confirm && [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]] || return
 
-ls /dev | grep ttyUSB
-read -p "Enter your port " portname
+checkTTY
+idf.py -p /dev/$answer monitor
 
-idf.py -p /dev/$portname monitor
+
+#ls /dev | grep ttyUSB
+#read -p "Enter your port " portname
+
+#idf.py -p /dev/$portname monitor
 
 
 
